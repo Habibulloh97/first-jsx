730d5675565a3d547815de363164d3a97a3d913b
Habibulloh97 <81605677+Habibulloh97@users.noreply.github.com>
2021-07-07 23:57:51 -0400

    first-jsx

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './first.jsx'
+import First from './first.jsx';
+
+function App() {
+  return (
+    <div class="Header">
+      <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/first.jsx
@@ -0,0 +1,21 @@
+    import logo from './logo.svg';
+    import './App.css';
+
+    function First() {
+    return (
+        <div>
+            <h1>Hello Dojo!</h1>
+            <div>
+                <h2>Things I need to do</h2>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Work hard towrds goals</li>
+                    <li>Be consitant</li>
+                </ul>
+            </div>
+        </div>
+        
+    );
+    }
+
+    export default First;
